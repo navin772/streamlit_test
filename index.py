fddfb2795e7c54632e39f093fa9c459b4bc9663d
navin772 <98466550+navin772@users.noreply.github.com>
2022-03-16 16:15:57 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
 import streamlit as st
-st.header("Hello World")
+st.set_page_config("First Streamlit web-app")
 
-num = st.number_input("Enter a number", 1, 10)
+st.header("Table printer")
+
+
+
+num = st.number_input("Enter a number", 1, 1000)
 
 for i in range (1,11):
 
     st.write(num,'X', i,'=', num*i)
+st.balloons()
